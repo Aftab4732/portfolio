73975b725ee43891fc442eaddb4ee773a7d1c6ef
Aftab <110122499+Aftab4732@users.noreply.github.com>
2025-04-15 15:38:56 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/Aftab1.png"
+import profilePic from "../assets/Aftab1.jpg"
 import { motion } from "framer-motion";
 
 const container=(delay)=>({
